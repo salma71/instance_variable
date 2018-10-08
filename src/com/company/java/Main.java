206533e0453ca53f1dfc66a1a46a512072cd831f
Salma Elshahawy <salmaeng71@gmail.com>
2018-10-08 09:18:00 -0400

    copy content of a file line by line

--- src/com/company/java/Main.java
+++ src/com/company/java/Main.java
@@ -4,38 +4,71 @@ import com.company.java.model.ClothingItem;
 import com.company.java.model.Product;
 import com.company.java.model.Shirt;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-//        ClothingItem item = new ClothingItem("shirt", "L", 19.99);
-//        System.out.println(item);
-// // you cannot call the setPattern() method here before the constructor because it is not a superclass
-//        displayProduct(item);
+        String sourceFile = "files/loremipsom.txt";
+        String targetFile =  "files/target.txt";
 
-        Shirt shirt = new Shirt("M", 14.99);
-        displayProduct(shirt);
-        shirt.setPattern("Plaid");
-        System.out.println("this shirt has a pattern of " + shirt.getPattern());
+        try (   FileReader fileReader = new FileReader(sourceFile);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                FileWriter writer = new FileWriter(targetFile);
+                ){
 
-        ClothingItem reallyAShirt = new Shirt("L", 18.99);
-        displayProduct(reallyAShirt);
 
-        Shirt shirt2 = (Shirt) reallyAShirt; // this is the upcasting syntax
-        // this because I treat a sub-class as a supper class.
-        // otherwise this will through an error
-        shirt2.setPattern("Solid");
-        System.out.println("pattern = " + shirt2.getPattern());
+            while (true) {
+                String line = bufferedReader.readLine();
+                if (line == null) {
+                    break;
+                }
+                writer.write(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    }
-
-    private static void displayProduct(Product product){
-        String output = product.getClass().getSimpleName() + "{" +
-                "type='" + product.getType() + '\'' +
-                ", size='" + product.getSize() + '\'' +
-                ", price=" + product.getPrice() +
-                '}';
-        System.out.println(output);
     }
 }
+
+//import com.company.java.model.ClothingItem;
+//        import com.company.java.model.Product;
+//        import com.company.java.model.Shirt;
+//
+//public class Main {
+//    public static void main(String[] args) {
+////        ClothingItem item = new ClothingItem("shirt", "L", 19.99);
+////        System.out.println(item);
+//// // you cannot call the setPattern() method here before the constructor because it is not a superclass
+////        displayProduct(item);
+//
+//        Shirt shirt = new Shirt("M", 14.99);
+//        displayProduct(shirt);
+//        shirt.setPattern("Plaid");
+//        System.out.println("this shirt has a pattern of " + shirt.getPattern());
+//
+//        ClothingItem reallyAShirt = new Shirt("L", 18.99);
+//        displayProduct(reallyAShirt);
+//
+//        Shirt shirt2 = (Shirt) reallyAShirt; // this is the upcasting syntax
+//        // this because I treat a sub-class as a supper class.
+//        // otherwise this will through an error
+//        shirt2.setPattern("Solid");
+//        System.out.println("pattern = " + shirt2.getPattern());
+//
+//    }
+//
+//    private static void displayProduct(Product product){
+//        String output = product.getClass().getSimpleName() + "{" +
+//                "type='" + product.getType() + '\'' +
+//                ", size='" + product.getSize() + '\'' +
+//                ", price=" + product.getPrice() +
+//                '}';
+//        System.out.println(output);
+//    }
+//}
+
+
 
 
 //package com.company.java;
